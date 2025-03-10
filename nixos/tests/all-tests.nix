827bb2dedec3e23b4ddd8be09da714570fbc2599
RatCornu <balthazar.patiachvili@crans.org>
2025-03-10 15:06:36 +0100

    nixos/szurubooru: add nixos test

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -1193,6 +1193,7 @@ in {
   systemd-homed = handleTest ./systemd-homed.nix {};
   systemtap = handleTest ./systemtap.nix {};
   startx = import ./startx.nix { inherit pkgs runTest; };
+  szurubooru = handleTest ./szurubooru.nix {};
   taler = handleTest ./taler {};
   tandoor-recipes = handleTest ./tandoor-recipes.nix {};
   tandoor-recipes-script-name = handleTest ./tandoor-recipes-script-name.nix {};
