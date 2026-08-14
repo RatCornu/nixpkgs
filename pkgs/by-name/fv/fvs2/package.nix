b6aae2dcdc83cb8ad6c54e87537575495994bba5
Gliczy <129636582+Gliczy@users.noreply.github.com>
2026-08-14 12:01:15 +0200

    fvs2: 0.9.0 -> 0.10.0

--- pkgs/by-name/fv/fvs2/package.nix
+++ pkgs/by-name/fv/fvs2/package.nix
@@ -8,22 +8,22 @@ let
   core = fetchFromGitHub {
     owner = "fvs-lab";
     repo = "core";
-    tag = "v0.1.1";
+    tag = "v0.1.2";
     hash = "sha256-vEQhV9wInqxgJlSyhgp0BV5VaYBJVtqcPrdN2NP33i4=";
   };
 in
 buildGoModule (finalAttrs: {
   pname = "fvs2";
-  version = "0.9.0";
+  version = "0.10.0";
 
   src = fetchFromGitHub {
     owner = "fvs-lab";
     repo = "fvs2";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-lJZd9QbvjArYMSXG6qHcaxaAe5UOPpko/TXD2L8yxDA=";
+    hash = "sha256-QUSAhHepM27ixwcbez0bSBRPMLXUpxjmfRYWJiAOH04=";
   };
 
-  vendorHash = "sha256-FUst0ccxpZS3NjvthQQE7CVlULWPhXj8KA2cOoksLGI=";
+  vendorHash = "sha256-9Zri8PkMZ7J5q2CxxOpOE0lrPmw1cSJjmvJurtWRnbM=";
 
   # Needed for build time tests
   nativeBuildInputs = [ git ];
